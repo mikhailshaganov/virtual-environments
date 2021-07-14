79ec38be736329fe6f8c98b47ce97a116183188d
mikhailshaganov <mikhailshaganov@github.com>
2021-07-14 18:21:23 +0300

    kotlin linux fixes

--- images/linux/scripts/installers/kotlin.sh
+++ images/linux/scripts/installers/kotlin.sh
@@ -4,9 +4,13 @@
 ##  Desc:  Installs Kotlin
 ################################################################################
 
+source $HELPER_SCRIPTS/install.sh
 # Install latest kotlin from releases
-URL=$(curl -s https://api.github.com/repos/JetBrains/kotlin/releases/latest | jq -r '.assets[].browser_download_url) | select(contains("kotlin-compiler"))')
-curl -L $URL -o /usr/local/bin/kotlin
-chmod +x /usr/local/bin/kotlin
+URL=$(curl -s https://api.github.com/repos/JetBrains/kotlin/releases/latest | jq -r '.assets[].browser_download_url | select(contains("kotlin-compiler"))')
+
+download_with_retries $URL "/tmp"
+unzip -qq /tmp/kotlin-compiler*.zip -d /tmp
+mv kotlinс /usr/local/bin
+chmod +x /usr/local/bin/kotlinс
 
 invoke_tests "Tools" "Kotlin"
