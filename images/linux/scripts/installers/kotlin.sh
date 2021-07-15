63efa206fd3f4a87b8f001860c2cbf2b7650d4d8
mikhailshaganov <mikhailshaganov@github.com>
2021-07-15 16:26:32 +0300

    fix ubuntu and macOS

--- images/linux/scripts/installers/kotlin.sh
+++ images/linux/scripts/installers/kotlin.sh
@@ -5,12 +5,19 @@
 ################################################################################
 
 source $HELPER_SCRIPTS/install.sh
-# Install latest kotlin from releases
-URL=$(curl -s https://api.github.com/repos/JetBrains/kotlin/releases/latest | jq -r '.assets[].browser_download_url | select(contains("kotlin-compiler"))')
 
+URL=$(curl -s https://api.github.com/repos/JetBrains/kotlin/releases/latest | jq -r '.assets[].browser_download_url | select(contains("kotlin-compiler"))')
 download_with_retries $URL "/tmp"
+
 unzip -qq /tmp/kotlin-compiler*.zip -d /tmp
-mv kotlinс /usr/local/bin
-chmod +x /usr/local/bin/kotlinс
+FILES="/tmp/kotlinc/bin/*"
+
+for f in $FILES
+do
+    if [[ "$f" != *".bat"* ]]; then
+      chmod +x "$f"
+      mv "$f" /usr/local/bin
+    fi
+done
 
 invoke_tests "Tools" "Kotlin"
