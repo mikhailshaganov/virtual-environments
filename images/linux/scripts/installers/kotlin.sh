6ecef4c6d26d15e1d8b37480a06d36fe6f361129
mikhailshaganov <mikhailshaganov@github.com>
2021-07-20 18:27:02 +0300

    brew kotlin

--- images/linux/scripts/installers/kotlin.sh
+++ images/linux/scripts/installers/kotlin.sh
@@ -6,18 +6,19 @@
 
 source $HELPER_SCRIPTS/install.sh
 
-URL=$(curl -s https://api.github.com/repos/JetBrains/kotlin/releases/latest | jq -r '.assets[].browser_download_url | select(contains("kotlin-compiler"))')
-download_with_retries $URL "/tmp"
+# URL=$(curl -s https://api.github.com/repos/JetBrains/kotlin/releases/latest | jq -r '.assets[].browser_download_url | select(contains("kotlin-compiler"))')
+# download_with_retries $URL "/tmp"
 
-unzip -qq /tmp/kotlin-compiler*.zip -d /tmp
-FILES="/tmp/kotlinc/bin/*"
+# unzip -qq /tmp/kotlin-compiler*.zip -d /tmp
+# FILES="/tmp/kotlinc/bin/*"
 
-for f in $FILES
-do
-    if [[ "$f" != *".bat"* ]]; then
-      chmod +x "$f"
-      mv "$f" /usr/local/bin
-    fi
-done
+# for f in $FILES
+# do
+#     if [[ "$f" != *".bat"* ]]; then
+#       chmod +x "$f"
+#       mv "$f" /usr/local/bin
+#     fi
+# done
 
+brew install kotlin
 invoke_tests "Tools" "Kotlin"
